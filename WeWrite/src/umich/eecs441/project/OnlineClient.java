d7257794839eb4e66ce78e3cd9f2c475d6717a33
Bingjie Xu <bingjiex@umich.edu>
2013-09-21 16:52:44 -0400

    onlineclient

--- WeWrite/src/umich/eecs441/project/OnlineClient.java
+++ WeWrite/src/umich/eecs441/project/OnlineClient.java
@@ -36,6 +36,7 @@ public class OnlineClient {
 	
 	// TODO expected to change to client type
 	private CollabrifyClient client;
+	
 	// set the context from your main activity
 	private Context context;	
 	// pass activity reference
@@ -43,17 +44,184 @@ public class OnlineClient {
 	// for the client
 	private CollabrifyListener collabrifyListener;
 	
+	// session ID
+	private long sessionId;
+	// session name
+	private String sessionName;
+	
+	
+	
+	
+	
 	
 	// initial with 0, tells if the two lists contains actions
 	private int commandStackContains;
-	
 	// !!! We still need to track the redo list since the instant operation will not
 	// put the command into redo list
 	private int redoListContains;
 	
 
 	// TODO expected to add constructor with client type argument
-	protected OnlineClient(Context contex, CollabrifyListener collabrifyListener) {
+	protected OnlineClient(Context contex) {
+		
+		collabrifyListener = new CollabrifyAdapter () {
+			@Override
+		    public void onDisconnect() {
+		        Log.i("client connection", "disconnected");
+		        activity.runOnUiThread(new Runnable(){
+
+		        	@Override
+		        	public void run() {
+		        	}
+		        });
+			}
+
+/*			@Override
+			public void onReceiveEvent(final long orderId, int subId,
+					String eventType, final byte[] data) {
+				
+				Log.d("client connection", "RECEIVED SUB ID:" + subId);
+				
+				activity.runOnUiThread(new Runnable() {
+					@Override
+		          	public void run() {
+					}
+				});
+			}
+*/
+/*			@Override
+			public void onReceiveSessionList(final List<CollabrifySession> sessionList) {
+	        
+				if( sessionList.isEmpty() ) {
+					Log.i("client connection", "No session available");
+					return;
+				}
+				List<String> sessionNames = new ArrayList<String>();
+				for( CollabrifySession s : sessionList ) {
+					sessionNames.add(s.name());
+				}
+				final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+				builder.setTitle("Choose Session").setItems(
+						sessionNames.toArray(new String[sessionList.size()]),
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								try {	
+										sessionId = sessionList.get(which).id();
+										sessionName = sessionList.get(which).name();
+										myClient.joinSession(sessionId, null);
+								} catch( CollabrifyException e ) {
+										Log.e(TAG, "error", e);
+								}
+							}
+						});
+
+				activity.runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+					}
+				});
+			}
+*/
+	      
+	      	@Override
+	      	public void onSessionCreated(long id) {
+	      		Log.i("client connection", "Session created, id: " + id);
+	      		sessionId = id;
+	      		activity.runOnUiThread(new Runnable() {
+
+	      			@Override
+	      			public void run() {
+
+	      			
+	      			}
+	      		});
+	      	}
+
+	      	@Override
+	      	public void onError(CollabrifyException e) {
+	      		Log.e("client connection", "error", e);
+	      	}
+/*
+	      	@Override
+	      	public void onSessionJoined(long maxOrderId, long baseFileSize) {
+	      		Log.i("client connection", "Session Joined");
+	      		if( baseFileSize > 0 ) {
+	      			//initialize buffer to receive base file
+	      			baseFileReceiveBuffer = new ByteArrayOutputStream((int) baseFileSize);
+	      		}
+	      		activity.runOnUiThread(new Runnable() {
+	      			
+	      			@Override
+	      			public void run() {
+	      			}
+	      		});
+	      	}
+	      	 
+*/	      	
+/*	      	@Override
+	      	public byte[] onBaseFileChunkRequested(long currentBaseFileSize) {
+	      		// read up to max chunk size at a time
+	      		byte[] temp = new byte[CollabrifyClient.MAX_BASE_FILE_CHUNK_SIZE];
+	      		int read = 0;
+	      		try {
+	      			read = baseFileBuffer.read(temp);
+	      		} catch( IOException e ) {
+	      			// TODO Auto-generated catch block
+	      			e.printStackTrace();
+	      		}
+	      		if( read == -1 ) {
+	      			return null;
+	      		}
+	      		if( read < CollabrifyClient.MAX_BASE_FILE_CHUNK_SIZE ) {
+	      			// Trim garbage data
+	      			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	      			bos.write(temp, 0, read);
+	      			temp = bos.toByteArray();
+	      		}
+	      		return temp;
+	      	}
+		       
+	      	@Override
+	      	public void onBaseFileChunkReceived(byte[] baseFileChunk) {
+	      		try {
+	      			if( baseFileChunk != null ) {
+	      				
+	      				baseFileReceiveBuffer.write(baseFileChunk);
+	      			} else {
+	      				activity.runOnUiThread(new Runnable() {
+	      					@Override
+	      					public void run() {
+	      						broadcastedText.setText(baseFileReceiveBuffer.toString());
+	      					}
+	      				});
+	      				baseFileReceiveBuffer.close();
+	      			}
+	      		} catch( IOException e ) {
+	      			// TODO Auto-generated catch block
+	      			e.printStackTrace();
+	      		}
+	      	}
+
+	      	 
+	      	@Override
+	      	public void onBaseFileUploadComplete(long baseFileSize)	{
+	      		
+		        activity.runOnUiThread(new Runnable() {
+
+		        	@Override
+		        	public void run() {
+		        	}
+		        });
+		        try {
+		        	baseFileBuffer.close();
+		        } catch( IOException e ) {
+		          // TODO Auto-generated catch block
+		        	e.printStackTrace();
+		        }
+		     }
+*/		};
+		
 		try {
 			client = new CollabrifyClient(context, "ssx@umich.edu", "Shao", 
 					"441fall2013@umich.edu", "XY3721425NoScOpE", false, collabrifyListener);
@@ -64,9 +232,9 @@ public class OnlineClient {
 	}
 	
 	
-	public static OnlineClient getInstance(Context context, CollabrifyListener collabrifyListener) {
+	public static OnlineClient getInstance(Context context) {
 		if (instance == null)
-			instance = new OnlineClient(context, collabrifyListener);
+			instance = new OnlineClient(context);
 		return instance;
 	}
 	
