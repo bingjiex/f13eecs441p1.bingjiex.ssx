7281dc4a50f1701cf3ac59317bca335ce0b2bbaf
Bingjie Xu <bingjiex@umich.edu>
2013-09-25 16:26:14 -0400

    main menu background pic added (bug fixed)

--- WeWrite/gen/edu/umich/imlc/collabrify/client/R.java
+++ WeWrite/gen/edu/umich/imlc/collabrify/client/R.java
@@ -8,8 +8,8 @@ package edu.umich.imlc.collabrify.client;
 
 public final class R {
 	public static final class drawable {
-		public static final int ic_action_search = 0x7f020001;
-		public static final int ic_launcher = 0x7f020002;
+		public static final int ic_action_search = 0x7f020002;
+		public static final int ic_launcher = 0x7f020003;
 	}
 	public static final class string {
 		public static final int app_name = 0x7f040000;
--- WeWrite/gen/umich/eecs441/project/R.java
+++ WeWrite/gen/umich/eecs441/project/R.java
@@ -21,10 +21,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int editorbackground=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int shadow=0x7f020003;
+        public static final int background=0x7f020000;
+        public static final int editorbackground=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int shadow=0x7f020004;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
@@ -102,6 +103,5 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
-        public static final int ButtonText=0x7f050002;
     }
 }
